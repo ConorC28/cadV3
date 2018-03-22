9418987931b03e60719035658b1c177bb15e42c6
ConorC28 <conlonconor@gmail.com>
2018-03-22 21:45:44 +0000

    Ability to add to postgresql db and display patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -8,14 +8,25 @@ class PatientsController < ApplicationController
 		
 	end
 	
+	
+	
 	def newpatient
 	
 	end
 	
-	def create
-		#render plain: params[:patient].inspect
+	def show
+		@patient = Patient.find(params[:id])		
 	end
 	
-		
+	def create
+		@patient = Patient.new(patient_params)
+		@patient.save
+		redirect_to @patient
+	end
+	
+	private def patient_params
+		params.require(:patient).permit(:firstname, :secondname, :dateofbirth, :address, :address, :phonenumber, :illness, :relevantmedicalinfo, :nextofkinfirstname, :nextofkinsecondname, :nextofkincontactnumber, :nextofkinaddress)
+	
+	end
 	
 end
