420438a9f64851c403a7ec6b049083a69f8c8177
ConorC28 <conlonconor@gmail.com>
2018-03-26 21:18:39 +0100

    destroy added

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,5 @@
 class PatientsController < ApplicationController
-	def patientspage
+	def patientspage								# PatientPage view elements
 		@title = 'Patients';
 		@patientspagecontent1 = 'Patients and the execution of service are the number one focus of the I.M.H. With good service and high quality 
 		data analysis we hope to create a healthier future for the people of Ireland. The patients section allows medical practitioners to update
@@ -10,36 +10,37 @@ class PatientsController < ApplicationController
 		
 	end
 	
-	def patientdb
+	def patientdb									# PatientDB view elements
 		@title = 'Patients Database';
 		@patientdbcontent1 = 'The patients database displays patient data and also allows for searching. It is intended for use by medical 
 		professional only.';
-		@patients = Patient.all
+		@patients = Patient.all						
 	end
 	
-	def newpatient
+	def newpatient									# New view elements
 		@title = 'Please add info about the new Patient';
 	end
 	
-	def show
+	def show										# Show view elements
 		@title = 'New Patient Added';
-		@patient = Patient.find(params[:id])		
+		@patient = Patient.find(params[:id])		# send the user to a page displaying the new patient added		
 	end
 	
-	def create
-		@patient = Patient.new(patient_params)
+	def create										# Method to create patient
+		@patient = Patient.new(params[:id])  	
 		@patient.save
-		redirect_to @patient
+		redirect_to @patients_path
 	end
+		
 	
-	def edit
+	def edit										# Edit view elements
 		@title = 'Update Patient Data'
 		@content = 'Welcome to the patients edit page, please ensure any changes made 
 		to patients details before saving.'
 		@patient = Patient.find(params[:id])
 	end
 	
-	def update		
+	def update										#method to update patient
 		@patient = Patient.find(params[:id])
 		if(@patient.update(patient_params))
 			redirect_to @patient
@@ -48,8 +49,16 @@ class PatientsController < ApplicationController
 		end
 	end
 	
+	def destroy
+		@patient = Patient.find(params[:id])
+		@patient.destroy
+		
+		render 'patientspage'		
+		
+	end
 	
-	private def patient_params
+	
+	private def patient_params						# Patient Params
 		params.require(:patient).permit(:firstname, :secondname, :dateofbirth, :address, :phonenumber, :illness, :relevantmedicalinfo, :nextofkinfirstname, :nextofkinsecondname, :nextofkincontactnumber, :nextofkinaddress)
 	
 	end
